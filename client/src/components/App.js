d18d5e605727cefe7a41cdeceb1e72fd687c058e
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-04-11 22:38:49 +0000

    Setup basic responsive sidebar with material ui

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,6 +1,17 @@
 import React from "react";
-import { BrowserRouter, Switch, Link, Route } from "react-router-dom";
-import axios from "axios";
+import clsx from "clsx";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Drawer from "@material-ui/core/Drawer";
+import Container from "@material-ui/core/Container";
+import Hidden from "@material-ui/core/Hidden";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
 
 import Calendar from "./Calendar";
 import Sidebar from "./Sidebar";
@@ -8,29 +19,121 @@ import Home from "./Home";
 import Stats from "./Stats";
 import Detail from "./Detail";
 
-export default function Application() {
-  // const [message, setMessage] = useState("");
+const drawerWidth = 200;
 
-  // const fetchData = () => {
-  //   axios.get("/api/data").then((response) => {
-  //     setMessage(response.data.message);
-  //   });
-  // };
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  drawerPaper: {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    paddingTop: theme.spacing(4),
+    paddingBottom: theme.spacing(4),
+    paddingLeft: theme.spacing(1),
+    background: "#535454",
+    color: "#fff",
+    [theme.breakpoints.up("md")]: {
+      width: 240,
+    },
+  },
+  appBar: {
+    [theme.breakpoints.down("sm")]: {
+      background: "#535454",
+      color: "#fff",
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  content: {
+    flexGrow: 1,
+    height: "100vh",
+    overflow: "auto",
+  },
+  container: {
+    paddingTop: theme.spacing(9),
+    paddingBottom: theme.spacing(4),
+    [theme.breakpoints.up("sm")]: {
+      paddingTop: theme.spacing(4),
+    },
+  },
+}));
+
+export default function App(props) {
+  const { container } = props;
+  const classes = useStyles();
+  const theme = useTheme();
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
   return (
     <BrowserRouter>
-      <div className="App">
-        {/* <h1>{message}</h1>
-        <div>Click to fetch data</div>
-        <button onClick={fetchData}>Fetch Data</button> */}
-        <Sidebar />
-
-        <Switch>
-          <Route exact path="/user/:userid/calendar" component={Calendar} />
-          <Route exact path="/user/:userid/stats" component={Stats} />
-          <Route exact path="/recipe/:recipeid" component={Detail} />
-          <Route exact path="/" component={Home} />
-        </Switch>
+      <div className={clsx("App", classes.root)}>
+        <CssBaseline />
+        <Hidden smUp>
+          <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+              <IconButton
+                color="inherit"
+                aria-label="open drawer"
+                edge="start"
+                onClick={handleDrawerToggle}
+                className={classes.menuButton}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography variant="h6" noWrap>
+                Yumeek
+              </Typography>
+            </Toolbar>
+          </AppBar>
+        </Hidden>
+        <Hidden smUp implementation="css">
+          <Drawer
+            container={container}
+            variant="temporary"
+            anchor={theme.direction === "rtl" ? "right" : "left"}
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true,
+            }}
+          >
+            <Sidebar />
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown>
+          <Drawer
+            variant="permanent"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+          >
+            <Sidebar />
+          </Drawer>
+        </Hidden>
+        <main className={classes.content}>
+          <Container maxWidth="lg" className={classes.container}>
+            <Switch>
+              <Route exact path="/user/:userid/calendar" component={Calendar} />
+              <Route exact path="/user/:userid/stats" component={Stats} />
+              <Route exact path="/recipe/:recipeid" component={Detail} />
+              <Route exact path="/" component={Home} />
+            </Switch>
+          </Container>
+        </main>
       </div>
     </BrowserRouter>
   );
