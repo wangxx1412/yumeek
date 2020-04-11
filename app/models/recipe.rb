a5966353e3a055e3747e798c12ab13cc7009e91a
Zhao Wang <wangz2266@gmail.com>
2020-04-11 14:41:00 -0700

    modify association between recipes and nutrients

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Recipe < ApplicationRecord
-  has_many :nutrients
+  has_one :nutrients
+  has_many :user_recipes
   has_many :users, through: :user_recipes
 end
