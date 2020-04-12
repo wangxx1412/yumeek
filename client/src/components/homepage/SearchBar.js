0e283371e13341ddfd33d9edca3b8ae76b0f63ae
Zhao Wang <wangz2266@gmail.com>
2020-04-12 10:51:02 -0700

    searchbar

--- client/src/components/homepage/SearchBar.js
+++ client/src/components/homepage/SearchBar.js
@@ -1,9 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import InputBase from "@material-ui/core/InputBase";
+import { fade, makeStyles } from "@material-ui/core/styles";
+import SearchIcon from "@material-ui/icons/Search";
+import Button from "@material-ui/core/Button";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  search: {
+    position: "relative",
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    "&:hover": {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginLeft: 0,
+    width: "100%",
+    [theme.breakpoints.up("sm")]: {
+      marginLeft: theme.spacing(1),
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    width: "100%",
+  },
+}));
 
 export default function SearchBar() {
+  const classes = useStyles();
+  const [searchValue, setsearchValue] = useState("");
+  const handleSearch = () => {
+    console.log(searchValue);
+    setsearchValue("");
+  };
+
   return (
-    <div>
-      <div>SearchBar</div>
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <Button
+            variant="contained"
+            size="medium"
+            color="primary"
+            onClick={handleSearch}
+          >
+            Go!
+          </Button>
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon button onClick={handleSearch} />
+            </div>
+            <InputBase
+              placeholder="Search…"
+              classes={{
+                root: classes.inputRoot,
+                input: classes.inputInput,
+              }}
+              inputProps={{ "aria-label": "search" }}
+              value={searchValue}
+              onChange={(e) => setsearchValue(e.target.value)}
+            />
+          </div>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
