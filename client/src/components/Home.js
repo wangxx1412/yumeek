d4ced973681a49b7ded63126744195d8e600d639
Zhao Wang <wangz2266@gmail.com>
2020-04-12 22:03:51 -0700

    handle search

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,20 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "./homepage/SearchBar";
 import RecipesCardsLists from "./homepage/RecipesCardsLists";
-
-import sampleRecipes from "./homepage/example-recipes"; //! sample
+import axios from "axios";
+import recipesDataHelper from "../helper/recipesDataHelper";
 
 // SearchBar, RecipesCardsLists
 export default function Home() {
+  const appInfo = `&app_id=${process.env.REACT_APP_APP_ID}&app_key=${process.env.REACT_APP_APP_KEY}&from=0&to=100`;
+  const apiBaseURL = `https://api.edamam.com/search?q=`;
+  const [searchResult, setSearchResult] = useState([]);
+  useEffect(() => {
+    const recipeList = [
+      "chicken",
+      "beef",
+      "fish",
+      "pepper",
+      "carrot",
+      "paste",
+      "french fries",
+      "ice cream",
+      "bread",
+      "pancakes",
+      "burger",
+      "pizza",
+      "appal pie",
+      "bagel",
+    ];
+    const randomRecipe =
+      recipeList[Math.floor(Math.random() * recipeList.length)];
+    axios
+      .get(apiBaseURL + randomRecipe + appInfo)
+      .then((response) => setSearchResult(recipesDataHelper(response)));
+  }, []);
+
   const handleSearch = (value) => {
-    //! search recipe to API
+    axios
+      .get(apiBaseURL + value + appInfo)
+      .then((response) => setSearchResult(recipesDataHelper(response)));
     console.log("search", value);
   };
 
   return (
     <div>
       <SearchBar handleSearch={(value) => handleSearch(value)} />
-      <RecipesCardsLists searchResultRecipes={sampleRecipes} />
+      <RecipesCardsLists searchResultRecipes={searchResult} />
     </div>
   );
 }
