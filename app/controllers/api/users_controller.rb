c177b11e010108f74347863616857d83681edbd6
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-04-14 12:24:36 -0700

    Logged in owners can fetch their datas from recipes and associations

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,4 +1,19 @@
 class Api::UsersController < ApplicationController
+  def show
+    if session[:user_id].to_s == params[:id]
+
+      @response = Recipe.find_by_sql(["
+      SELECT Recipes.id, label, weekday, energies, carbs, fat, protein, fiber, img_url, src_url, health_labels, ingredients 
+      FROM Recipes 
+      Right OUTER JOIN Nutrients ON Recipes.id=Nutrients.recipe_id 
+      join user_recipes on Recipes.id=user_recipes.recipe_id where user_recipes.user_id=?",params[:id]])
+
+      render :json => { :data => @response }
+    else 
+      render :json => { :error => "Only owner can view the dashboard" }
+    end
+  end
+
   def create
     user = User.new(user_params)
     if user.save
@@ -16,3 +31,4 @@ class Api::UsersController < ApplicationController
     params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
+
