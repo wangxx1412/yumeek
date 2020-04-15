64ee0ad874d62ec7c26413d0415e9f11ea66039c
Xiaoxuan Wang <wangxiaoxuan4869@gmail.com>
2020-04-15 04:06:21 +0000

    Add routes for fetching week general datas

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,6 @@
 class Api::UsersController < ApplicationController
   def show
     if session[:user_id].to_s == params[:id]
-
       @response = Recipe.find_by_sql(["
       SELECT Recipes.id, label, weekday, energies, carbs, fat, protein, fiber, img_url, src_url, health_labels, ingredients 
       FROM Recipes 
@@ -30,9 +29,27 @@ class Api::UsersController < ApplicationController
     render :json => { :user_id => session[:user_id] }
   end
   
+  def fetchweek
+    if session[:user_id].to_s == params[:id]
+      @response = Recipe.find_by_sql(["
+      SELECT weekday, sum(energies) as energies,sum(carbs) as carbs, sum(fat) as fat, sum(protein) as protein, sum(fiber) as fiber
+      FROM Recipes 
+      Right OUTER JOIN Nutrients ON Recipes.id=Nutrients.recipe_id 
+      join user_recipes on Recipes.id=user_recipes.recipe_id 
+      where user_recipes.user_id=?
+      group by weekday",params[:id]])
+
+      render :json => { :data => @response }
+    else 
+      render :json => { :error => "Only owner can view the dashboard" }
+    end
+  end
   private
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
   end
 end
 
+
+
+      
