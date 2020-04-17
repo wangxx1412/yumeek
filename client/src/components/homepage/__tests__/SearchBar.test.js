52305e0662b8ee225cea90d7fa26e482b8744fb8
Zhao Wang <wangz2266@gmail.com>
2020-04-17 16:32:08 -0700

    add search bra test

---
+++ client/src/components/homepage/__tests__/SearchBar.test.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
+import SearchBar from "../SearchBar";
+
+afterEach(cleanup);
+
+describe("SearchBar", () => {
+  it("renders without crashing", () => {
+    render(<SearchBar />);
+  });
+
+  it("renders a 'Go!' button", () => {
+    const { getByText } = render(<SearchBar />);
+
+    expect(getByText("Go!")).toBeInTheDocument();
+  });
+
+  it("renders health labels", () => {
+    const { getByText } = render(<SearchBar />);
+
+    expect(getByText("vegetarian")).toBeInTheDocument();
+    expect(getByText("vegan")).toBeInTheDocument();
+    expect(getByText("alcohol-free")).toBeInTheDocument();
+    expect(getByText("tree-nut-free")).toBeInTheDocument();
+    expect(getByText("peanut-free")).toBeInTheDocument();
+    expect(getByText("sugar-conscious")).toBeInTheDocument();
+  });
+
+  it("calls the handleSearch function when clicked the 'Go!' button", () => {
+    const handleSearch = jest.fn();
+
+    const { getByText } = render(<SearchBar handleSearch={handleSearch} />);
+
+    const button = getByText("Go!");
+
+    fireEvent.click(button);
+
+    expect(handleSearch).toHaveBeenCalledTimes(1);
+    expect(handleSearch).toHaveBeenCalledWith("", {
+      vegetarian: false,
+      vegan: false,
+      "alcohol-free": false,
+      "tree-nut-free": false,
+      "peanut-free": false,
+      "sugar-conscious": false,
+    });
+  });
+});
